1631cafe945c62585229bbbd995854e004cc72e5
Vlascitchii-Mihail <mishavlascitchii@gmail.com>
2022-06-27 10:33:19 +0300

    myCommit

--- Android/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.kt
+++ Android/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.kt
@@ -17,8 +17,6 @@ class TimePickerFragment: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreate(savedInstanceState)
 
-
-
         val calendar = Calendar.getInstance()
         val initialHours = calendar.get(Calendar.HOUR)
         val initialMinutes = calendar.get(Calendar.MINUTE)
