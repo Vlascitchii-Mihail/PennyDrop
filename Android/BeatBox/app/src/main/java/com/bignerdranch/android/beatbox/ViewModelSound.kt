519bcb1ea19ed7cf7dc047e8659cd0cea5c9344b
Vlascitchii-Mihail <mishavlascitchii@gmail.com>
2022-08-27 15:10:54 +0300

    Themes

--- Android/BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/ViewModelSound.kt
+++ Android/BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/ViewModelSound.kt
@@ -4,8 +4,15 @@ import androidx.lifecycle.ViewModel
 import android.content.res.AssetManager
 import kotlin.coroutines.coroutineContext
 
-class ViewModelSound: ViewModel() {
-    val beatBox: BeatBox = BeatBox(MainActivity().assets)
+class ViewModelSound (assets: AssetManager): ViewModel() {
+    var beatBox = BeatBox(assets)
 
+    fun saveBeat(beat: BeatBox) {
+        beatBox = beat
+    }
 
+    override fun onCleared() {
+        super.onCleared()
+        beatBox.release()
+    }
 }
