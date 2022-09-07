86756317cb3b7a19a17aafed2518cfbeb68443d1
Vlascitchii-Mihail <mishavlascitchii@gmail.com>
2022-09-07 15:34:38 +0300

    Penny

--- Android/Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ Android/Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var skyView: View
     private var sunPositionUp = true
 
+    //initialization the colors
     private val blueSkyColor: Int by lazy {
         ContextCompat.getColor(this, R.color.blue_sky)
     }
@@ -34,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         sunView = findViewById(R.id.sun)
         skyView = findViewById(R.id.sky)
 
+        //starting the animation when user clicked on the screen
         sceneView.setOnClickListener {
 //            reverseAnimation()
             sunPositionUp = if (sunPositionUp) {
@@ -46,26 +48,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //setting animation
     private fun startAnimation() {
+
+        //start and finish positions of the sun
+        /**
+         * @param top, bottom, right, left - inner parameters of the View
+         * @param hight - inner value - top minus bottom
+         */
         val sunYStart = sunView.top.toFloat()
         val sunYEnd = skyView.height.toFloat()
 
+        //creating the Animator to perform the animation - interpolation
         val heightAnimation = ObjectAnimator.ofFloat(sunView, "y", sunYStart, sunYEnd)
             .setDuration(3000)
+
+        //AccelerateInterpolator() - smooth animation
         heightAnimation.interpolator = AccelerateInterpolator()
 
+        //changing the sky's color
         val sunsetSkyAnimator = ObjectAnimator.ofInt(skyView, "backgroundColor", blueSkyColor,
         sunsetSkyColor).setDuration(3000)
+
+        //setEvaluator(ArgbEvaluator()) - smooth color transition, announces transit colors between some colors
         sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
 
+        //changing the sky's color
         val nightSkyAnimator = ObjectAnimator.ofInt(skyView, "backgroundColor", sunsetSkyColor,
         nightSkyColor).setDuration(1500)
+
+        //setEvaluator(ArgbEvaluator()) - smooth color transition, announces transit colors between some colors
         nightSkyAnimator.setEvaluator(ArgbEvaluator())
 
 //        heightAnimation.start()
 //        sunsetSkyAnimator.start()
 
+        //AnimatorSet() - animations set, setting the launching sequence
         val animatorSet = AnimatorSet()
+
+        //playing the animation
         animatorSet.play(heightAnimation).with(sunsetSkyAnimator).before(nightSkyAnimator)
         animatorSet.start()
     }
