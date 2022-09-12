06f27ed923af1dae0c0bf46c3d8cf382bf1e681f
Vlascitchii-Mihail <mishavlascitchii@gmail.com>
2022-09-12 16:18:06 +0300

    Sunset

--- Android/GeoQuiz_2/app/src/main/java/com/bignerdranch/android/geoquiz_2/Question.kt
+++
@@ -1,8 +0,0 @@
-package com.bignerdranch.android.geoquiz_2
-
-import androidx.annotation.StringRes
-
-//@StringRes помогает встроенному в Android Studio инспектору кода (под
-//названием Lint) проверить во время компиляции, что в
-//конструкторе используется правильный строковый идентификатор ресурса
-data class Question(@StringRes val textResId: Int, val answer: Boolean)
